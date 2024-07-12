68e250200cd9a126090b41cdc8eac45a442e6aec
beananaman453 <powelbenjamin05@gmail.com>
2024-07-12 08:42:40 -0500

    vomwefewf

--- src/health_potions.java
+++ src/health_potions.java
@@ -1,7 +1,7 @@
 public class health_potions extends Items {
     public String Iname;
     public int power;
-    public health_potions(String Iname, int power) {
-        super(Iname, power);
+    public health_potions(String Iname, int power, int prob,int prob2,int  dmg) {
+        super(Iname, power, prob, prob2, dmg);
     }
 }
